1534b3dd27323e2950a3e3493d0c98adc78c7bd4
Matko <matkoscab752@gmail.com>
2026-04-20 18:27:22 -0300

    mandando cambios de matko a dev

--- js/script.js
+++ js/script.js
@@ -5,15 +5,13 @@ const btnRGB = document.querySelector("#btnRGB");
 const r = document.querySelector("#r");
 const g = document.querySelector("#g");
 const b = document.querySelector("#b");
-const rValue = document.querySelector("#rValue")
-const gValue = document.querySelector("#gValue")
-const bValue = document.querySelector("#bValue")
-
-
+const rValue = document.querySelector("#rValue");
+const gValue = document.querySelector("#gValue");
+const bValue = document.querySelector("#bValue");
 
 btnRGB.addEventListener("click", () => {
   const color = `rgb(${r.value}, ${g.value}, ${b.value})`;
-  box.style.backgroundColor = color;
+  document.body.style.backgroundColor = color;
 
   const li = document.createElement("li");
   li.textContent = color;
@@ -24,7 +22,7 @@ btnRGB.addEventListener("click", () => {
 
 btnPicker.addEventListener("click", () => {
   const color = colorPicker.value;
-  box.style.backgroundColor = color;
+  document.body.style.backgroundColor = color;
 
   const li = document.createElement("li");
   li.textContent = color;
@@ -33,49 +31,48 @@ btnPicker.addEventListener("click", () => {
   lista.appendChild(li);
 });
 
-r.addEventListener( "input", () => {
-    rValue.textContent = r.value;
-})
+r.addEventListener("input", () => {
+  rValue.textContent = r.value;
+});
 
-r.addEventListener( "input", () => {
-  const valor = r.value
-    r.style.background = `
+r.addEventListener("input", () => {
+  const valor = r.value;
+  r.style.background = `
     linear-gradient(
       to right,
       rgb(0,0,0),
       rgb(255,0,0)
     )
   `;
+});
 
-})
-
-g.addEventListener( "input", () => {
-    gValue.textContent = g.value;
-})
-g.addEventListener( "input", () => {
-  const valor = g.value
-    g.style.background = `
+g.addEventListener("input", () => {
+  gValue.textContent = g.value;
+});
+g.addEventListener("input", () => {
+  const valor = g.value;
+  g.style.background = `
     linear-gradient(
       to right,
       rgb(0,0,0),
       rgb(0,255,0)
     )
    `;
-})
+});
 
-b.addEventListener( "input", () => {
-    bValue.textContent = b.value;
-})
-b.addEventListener( "input", () => {
-  const valor = b.value
-    b.style.background = `
+b.addEventListener("input", () => {
+  bValue.textContent = b.value;
+});
+b.addEventListener("input", () => {
+  const valor = b.value;
+  b.style.background = `
     linear-gradient(
       to right,
       rgb(0,0,0),
       rgb(0,0,255)
     )
    `;
-})
+});
 
 function pintarSliders() {
   r.style.background = `linear-gradient(to right, rgb(0,0,0), rgb(255,0,0))`;
@@ -90,11 +87,9 @@ function actualizarColor() {
   const gVal = g.value;
   const bVal = b.value;
 
-  box.style.background = `rgb(${rVal}, ${gVal}, ${bVal})`;
+  document.body.style.background = `rgb(${rVal}, ${gVal}, ${bVal})`;
 }
 
 r.addEventListener("input", actualizarColor);
 g.addEventListener("input", actualizarColor);
 b.addEventListener("input", actualizarColor);
-
-

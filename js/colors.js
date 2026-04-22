95b2ba30062576cba6a7dafda0e0881e5ff38aa1
FeverDream999 <you@example.com>
2026-04-22 19:11:14 -0300

    misc:commit to change branches

--- js/colors.js
+++ js/colors.js
@@ -1,13 +1,13 @@
 const expandButton = document.getElementById('expand');
 const div = document.getElementById('red');
 const diva = document.getElementById('blue');
-const circle1 = document.getElementById('green');
+const green = document.getElementById('green');
 
 expandButton.addEventListener('click', () => 
 {
   div.classList.toggle('red-expansion');
   diva.classList.toggle('blue-expansion');
-  circle1.classList.toggle('color-expansion');
+  green.classList.toggle('green-expansion');
 
   expandButton.remove();
 });

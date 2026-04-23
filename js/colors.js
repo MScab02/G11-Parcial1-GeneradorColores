a03fe54732874d86cd19c26bac12037f804d4103
FeverDream999 <you@example.com>
2026-04-23 12:55:30 -0300

    feat:new animation

--- js/colors.js
+++ js/colors.js
@@ -1,3 +1,5 @@
+
+
 const expandButton = document.getElementById('expand');
 const div = document.getElementById('red');
 const diva = document.getElementById('blue');
@@ -5,9 +7,12 @@ const green = document.getElementById('green');
 
 expandButton.addEventListener('click', () => 
 {
+
   div.classList.toggle('red-expansion');
   diva.classList.toggle('blue-expansion');
   green.classList.toggle('green-expansion');
+
+  circleElement.style.transform = '';
 
   expandButton.remove();
 });
